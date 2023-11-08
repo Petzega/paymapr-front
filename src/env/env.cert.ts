075f6c108f80c0ae773c0e063a7ccfc7cd387b0f
pzegarra <pzegarra@proteccion.com.co>
2023-11-08 17:40:51 -0500

    se cambia url

--- src/env/env.cert.ts
+++ src/env/env.cert.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/owner',
-    // apiUrl: 'https://srv-paymapr.fly.dev/owner',
+    // apiUrl: 'http://localhost:8080/owner',
+    apiUrl: 'https://srv-paymapr.fly.dev/owner',
     path: {
       person: "/person",
       payment: "/payment",
